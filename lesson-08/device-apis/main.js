6d3e71030c1b7c1f2d29d42d5e10d6254a0d3bd6
Kasia Laniecka <kl@parkshare.dk>
2022-04-01 09:37:43 +0200

    geolocation

--- lesson-08/device-apis/main.js
+++ lesson-08/device-apis/main.js
@@ -1,5 +1,28 @@
+const geolocation = document.querySelector("#geolocation");
+let latitude;
+let longitude;
+
 for (const button of document.querySelectorAll("button[id]")) {
   button.addEventListener("click", () => {
-    alert("Not implemented yet.");
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        getLocation();
+      });
+    } else {
+      console.log("doesn't work");
+    }
   });
+}
+async function getLocation() {
+  const url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    geolocation.innerHTML = await data.locality;
+  } catch (error) {
+    console.log(error);
+    geolocation.innerHTML = "Not found.";
+  }
 }
